78406819ad535e7d1ac86d3761571fe6a986d118
moothyknight <crazykid4534@gmail.com>
2019-09-17 00:52:57 -0800

    canvas demo on /listen page
    many more to come!

--- HEG_WIFI_BLE/evs.h
+++ HEG_WIFI_BLE/evs.h
@@ -3,15 +3,32 @@ const char event_page[] PROGMEM = R"=====(
 <html>
 <head>
 <style>
-body {
-    background-color: gray
-}
-scoreDiv {
-    color: blue
-}
+  body {
+      background-color: gray
+  }
+  eventDiv {
+      color: chartreuse
+  }
+  scoreDiv {
+      color: tomato
+  }
 </style>
 </head>
 <body id="main_body">
+
+    <div id="HEGAPI">     
+      <form method="post" action="/startHEG" target="dummyframe">
+          <button type="submit">Start HEG</button>
+      </form>
+      <form method="post" action="/stopHEG" target="dummyframe">
+          <button type="submit">Stop HEG</button>
+      </form>
+    
+    </div>
+
+    <div id="canvasContainer">
+        <canvas id="myCanvas" height="400px" width="400px"></canvas>
+    </div>
 
     <script>
         var ms = [];
@@ -21,7 +38,7 @@ scoreDiv {
         var smallSavLay = [];
         var largeSavLay = [];
         var adcAvg = [];
-        var ratioSlope = [];
+        var ratioSlope = [0];
         var AI = [];
         var slowFastScore = [100];
         var slowSMAArray = [];
@@ -83,30 +100,46 @@ scoreDiv {
 
         var containerHTML = '<div id="container"></div>';
         var messageHTML = '<div id="message">Not connected</div>';
-        var eventHTML = '<div id="myevent">Waiting...</div>';
+        var eventHTML = '<eventDiv id="myevent">Waiting...</eventDiv>';
         var scoreHTML = '<scoreDiv id="ScoreHTML">Getting score...</scoreDiv>';
 
-        appendFragment(containerHTML,"main_body");
+        appendFragment(containerHTML,"HEGAPI");
         appendFragment(messageHTML,"container");
         appendFragment(eventHTML,"container");
         appendFragment(scoreHTML,"container");
+
+        //var data;
+        //function getMessage(){
+        //    return data;
+        //}
 
         if (!!window.EventSource) {
             var source = new EventSource('/events');
 
             source.addEventListener('open', function(e) {
-                console.log("Events Connected");
+                console.log("HEGDUINO", "Events Connected");
             }, false);
 
             source.addEventListener('error', function(e) {
                 if (e.target.readyState != EventSource.OPEN) {
-                console.log("Events Disconnected");
+                  console.log("HEGDUINO", "Events Disconnected");
                 }
             }, false);
 
             source.addEventListener('message', function(e) {
                 document.getElementById("message").innerHTML = e.data;
-                console.log("message", e.data);
+                console.log("HEGDUINO", e.data);
+
+                <!-- Android interface -->
+                 // data = e.data;
+                 // if (typeof(Storage) !== "undefined"){window.localStorage.setItem('StateChangerPlugin', data);}  
+                 // if (typeof(StateChangerPlugin) !== "undefined"){StateChangerPlugin.message(data);} 
+                <!-- Android interface -->
+                <!-- Web interface -->
+                  // var onRead = new CustomEvent('on_read', { detail: {data: data} });
+                  // window.parent.dispatchEvent(onRead); 
+                  //window.parent.postMessage(data, "*");
+                <!-- Web interface -->
             }, false);
 
             source.addEventListener('myevent', function(e) {
@@ -125,28 +158,60 @@ scoreDiv {
                     AI.push(parseFloat(dataArray[8]));
 
                     //slowFastSMAScore(ratio);
-                    document.getElementById("ScoreHTML").innerHTML = ratio[ratio.length-1];//slowFastScore[slowFastScore.length - 1]
+                    document.getElementById("ScoreHTML").innerHTML = largeSavLay[largeSavLay.length-1];//slowFastScore[slowFastScore.length - 1]
                 }
+            }, false);
+
+            //data = e.data;
+            //if(typeof(Storage) !=="undefined") 
+            //{window.localStorage.setItem('StateChangerPlugin', data);}
+            //StateChangerPlugin.message(data);
             
-        }, false);
-
-        //data = e.data;
-        //if(typeof(Storage) !=="undefined") 
-        //{window.localStorage.setItem('StateChangerPlugin', data);}
-        //StateChangerPlugin.message(data);
         }
-    </script>
 
-    <form method="post" action="/startHEG" target="dummyframe">
-        <button type="submit">Start HEG</button>
-    </form>
-    <form method="post" action="/stopHEG" target="dummyframe">
-        <button type="submit">Stop HEG</button>
-    </form>
+      var mainCanvas = document.getElementById("myCanvas");
+      var mainContext = mainCanvas.getContext('2d');
+       
+      var canvasWidth = mainCanvas.width;
+      var canvasHeight = mainCanvas.height;
+       
+      var angle = 1.57;
+       
+      var requestAnimationFrame = window.requestAnimationFrame || 
+                                  window.mozRequestAnimationFrame || 
+                                  window.webkitRequestAnimationFrame || 
+                                  window.msRequestAnimationFrame;
+       
+      function drawCircle() {
+          mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+           
+          // color in the background
+          mainContext.fillStyle = "#2751CE";
+          mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+           
+          // draw the circle
+          mainContext.beginPath();
+           
+          var radius = 25 + 150 * Math.abs(Math.cos(angle));
+          mainContext.arc(200, 200, radius, 0, Math.PI * 2, false);
+          mainContext.closePath();
+           
+          // color in the circle
+          mainContext.fillStyle = "#EE1818";
+          mainContext.fill();
 
-    <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+          angleChange = ratioSlope[ratioSlope.length-1]
+          if(((angle > 1.57) || (angleChange > 0)) && ((angle < 3.14) || (angleChange < 0))) {
+            angle += angleChange*0.2;
+          }
+          requestAnimationFrame(drawCircle);
+      }
+      drawCircle();
+      
+  </script>
 
-
+  <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+   
 </body>
 </html>
 )=====";
