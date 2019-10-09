8a5ec59088dca9bf49cfd34def6a7cb5dbe321de
Bubba Skrimp <brewster.joshua1@gmail.com>
2019-10-08 16:40:25 -0800

    improve connect page

--- HEG_WIFI_BLE/connect.h
+++ HEG_WIFI_BLE/connect.h
@@ -38,15 +38,16 @@ input[type=submit]{
       <input type='text' id='ssid' name='ssid'><br>
     Password:<br>
       <input type='password' id='pw' name='pw'><br>
+    For use with Static IP: <br>
     Static IP (e.g. 192.168.0.199):<br>
       <input type='text' id='static' name='static'><br>
     Gateway IP (e.g. 192.168.0.1):<br>
       <input type='text' id='gateway' name='gateway'><br>
     Subnet Mask (e.g. 255.255.255.0):<br>
       <input type='text' id='subnet' name='subnet'><br>
-      <input type='checkbox' id='use_static' name='use_static'>Use Static IP<br>
-      <input type='checkbox' id='AP_ONLY' name='AP_ONLY'>Access Point Only<br>
-      <input type='checkbox' id'btSwitch' name='btSwitch'>Use Bluetooth<br>
+      <input type='radio' id='use_static' name='use_static'>Use Static IP<br>
+      <input type='radio' id='AP_ONLY' name='AP_ONLY'>Access Point Only<br>
+      <input type='radio' id'btSwitch' name='btSwitch'>Use Bluetooth<br>
     <input type='submit' id='Connect' value='Connect'>
   </form>
 )=====";
