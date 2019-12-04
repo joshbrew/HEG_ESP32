013e3fbf7cd43bba51abf8543bee105421f754ae
Bubba Skrimp <brewster.joshua1@gmail.com>
2019-12-03 15:40:23 -0900

    changes

--- HEG_WIFI_BLE/help.h
+++ HEG_WIFI_BLE/help.h
@@ -103,56 +103,56 @@ const char help_page[] PROGMEM= R"=====(
         <h4> Welcome to HEG Biofeedback with the Internet of Things! </h4>
         </h3>
         <p>
-        This hardware and software package is designed to have a minimum barrier 
-        for entry for users wanting to use or develop this unique form of brain training. 
-        Hemoencephalography (HEG) simply means using blood flow signals for feedback
-        data from the body while doing certain actions or experiencing different mental
-        states.
-        
-        Hemo - Meaning blood.
-        Encephalography - Imaging brain structure through a particular medium.
-        
-        This device you are using now belongs to particular classification called fNIRS
-        or "functional Near-Infrared Spectroscopy" as we are using Red and InfraRed LEDs
-        reflected into a light sensor to measure an important component of brain activity.
-        It is a reflectance-based pulse oximeter in closest comparison, and we can gather
-        the same data, we just use it in a "functional" way to measure overall O2 saturation
-        of a particular region of your brain.
-
-        The HEG sensor is able to capture signals from about an inch deep in your skull
-        wherever it has been placed. This is done mainly on our forehead and the pre-frontal
-        cortex as this is the most accessible area, but smaller sensors will allow for 
-        measuring other parts of the scalp. 
-        
-        The primary usage of these sensors in our case is for direct aerobic exercise for 
-        your brain. Normally you energize your brain through normal diet and exercise -
-        which add fuel and flexibility to your neurons. Aerobic exercise encourages good
-        blood flow and stronger capillary development. It not only stretches your back out
-        and loosens your limbs up - but your brain too! This causes energy to be more
-        available to your system in general and for it to get more used to stress and strain.
-
-        Our brain cells require energy to be immediately available to fire. Neurons receive
-        their energy on-demand through astrocytes connected to innumerable tiny capillary tubes
-        that wrap around your cells in order to sustain them. Stress, poor diet, dehydration,
-        or any number of things that can reduce your well-being have some of their strongest
-        impacts on brain health due to the incredibly sensitive and intricate ecosystem they form
-        in your noggin. 
-        
-        We can see correlations with poor health and poor state of mind directly with HEG by 
-        massive reductions or even over-activity in normal blood flow levels, as well as improving 
-        it through HEG-assisted exercise. This has overall benefits to your body and mind - which 
-        are not really separate. The main benefits are you will feel more aware, quicker on your 
-        feet, and able to make better choices when your brain is in shape. The directness of this 
-        procedure seems to extend the benefits of normal exercise to your brain in very profound 
-        ways, and may shed light some important ideas about the nature of living consciousness and 
-        memory.
-        
-        Before you embark, however, it is important to understand the risks and the experimental 
-        nature of this work. While general exposure to the HEG is safe, you can over-train and give 
-        yourself headaches or fatigue - kind of like normal exhaustion after a hard exercise. So 
-        please learn our recommendations and some of the known side-effects when you are first 
-        starting with HEG brain exercises.
-
+        This hardware and software package is designed to have a minimum barrier <br>
+        for entry for users wanting to use or develop this unique form of brain training. <br>
+        Hemoencephalography (HEG) simply means using blood flow signals for feedback<br>
+        data from the body while doing certain actions or experiencing different mental<br>
+        states.<br>
+        <br>
+        Hemo - Meaning blood.<br>
+        Encephalography - Imaging brain structure through a particular medium.<br>
+        <br>
+        This device you are using now belongs to particular classification called fNIRS<br>
+        or "functional Near-Infrared Spectroscopy" as we are using Red and InfraRed LEDs<br>
+        reflected into a light sensor to measure an important component of brain activity.<br>
+        It is a reflectance-based pulse oximeter in closest comparison, and we can gather<br>
+        the same data, we just use it in a "functional" way to measure overall O2 saturation<br>
+        of a particular region of your brain.<br>
+        <br>
+        The HEG sensor is able to capture signals from about an inch deep in your skull<br>
+        wherever it has been placed. This is done mainly on our forehead and the pre-frontal<br>
+        cortex as this is the most accessible area, but smaller sensors will allow for <br>
+        measuring other parts of the scalp. <br>
+        <br>
+        The primary usage of these sensors in our case is for direct aerobic exercise for <br>
+        your brain. Normally you energize your brain through normal diet and exercise -<br>
+        which add fuel and flexibility to your neurons. Aerobic exercise encourages good<br>
+        blood flow and stronger capillary development. It not only stretches your back out<br>
+        and loosens your limbs up - but your brain too! This causes energy to be more<br>
+        available to your system in general and for it to get more used to stress and strain.<br>
+        <br>
+        Our brain cells require energy to be immediately available to fire. Neurons receive<br>
+        their energy on-demand through astrocytes connected to innumerable tiny capillary tubes<br>
+        that wrap around your cells in order to sustain them. Stress, poor diet, dehydration,<br>
+        or any number of things that can reduce your well-being have some of their strongest<br>
+        impacts on brain health due to the incredibly sensitive and intricate ecosystem they form<br>
+        in your noggin. <br>
+        <br>
+        We can see correlations with poor health and poor state of mind directly with HEG by <br>
+        massive reductions or even over-activity in normal blood flow levels, as well as improving <br>
+        it through HEG-assisted exercise. This has overall benefits to your body and mind - which <br>
+        are not really separate. The main benefits are you will feel more aware, quicker on your <br>
+        feet, and able to make better choices when your brain is in shape. The directness of this <br>
+        procedure seems to extend the benefits of normal exercise to your brain in very profound <br>
+        ways, and may shed light some important ideas about the nature of living consciousness and <br>
+        memory.<br>
+        <br>
+        Before you embark, however, it is important to understand the risks and the experimental <br>
+        nature of this work. While general exposure to the HEG is safe, you can over-train and give <br>
+        yourself headaches or fatigue - kind of like normal exhaustion after a hard exercise. So <br>
+        please learn our recommendations and some of the known side-effects when you are first <br>
+        starting with HEG brain exercises.<br>
+        <br>
         </p>
         </div>
         <div id="s2">
