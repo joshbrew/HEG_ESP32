1a4718988711b608c82530113796df9dffaf5469
Bubba Skrimp <brewster.joshua1@gmail.com>
2019-12-16 17:57:42 -0900

    api page now easier to use for remote access. See info in code

--- HEG_WIFI_BLE/webDemo/api.h
+++ HEG_WIFI_BLE/webDemo/api.h
@@ -1,5 +1,5 @@
 const char api[] PROGMEM = R"=====(
-<!-- To start, try "var s = new HEGwebAPI("test", true)" in your console and see what happens! -->
+<!-- To start, while connected to your device's access point try "var s = new HEGwebAPI('test', true, 'http://192.168.4.1')" in your console and see what happens! -->
 <link rel="stylesheet" type="text/css" href="webDemoCSS.css">
 <script src="HEGwebAPI.js"></script>
 <!-- Refer to connect.html and update.html for correct POST format to control connection settings and updates. -->
